5855a196ace1590e1f61d8a3b94995be4b2488cf
Mayank Yadav <myyadavmayank1998@gmail.com>
2021-10-18 20:21:02 +0530

    fixed few errors 2

--- lib/pages/book_details_page.dart
+++ lib/pages/book_details_page.dart
@@ -5,6 +5,8 @@ class BookDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: const Text("jdbbscjk"),
+    );
   }
 }
